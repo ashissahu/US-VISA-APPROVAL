7c0a82efe9959075278462902440947e9a4f18c9
Ashis Kumar Sahu <ashisml302@gmail.com>
2024-09-26 20:47:09 +0530

    updated requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.1.0",
+    author="Ashis",
+    author_email="ashisml302@gmail.com",
+    packages=find_packages()
+)
